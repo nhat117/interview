949d186f40ee9011216de4b8b75782a2fe3940fd
nhat117 <s3878174@rmit.edu.vn>
2022-10-24 16:23:37 +0700

    Add scrolling

--- pages/chats.jsx
+++ pages/chats.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import axios from "axios";
 import ChatCells from "../components/ChatCells";
 import ChatInputs from "../components/ChatInputs";
@@ -8,6 +8,12 @@ const path = "http://localhost:3000/messages";
 const chats = () => {
 	const [messages, setMessages] = useState([]);
 	const [sendMessage, setSendMessage] = useState("");
+    const divRef = useRef(null);
+    
+    useEffect(() => {
+        divRef.current.scrollIntoView({ behavior: 'smooth' });
+    });
+
 
 	const fetchMessages = async () => {
 		const tmpMsg = localStorage.getItem("messages");
@@ -15,14 +21,13 @@ const chats = () => {
 			console.log("fetching messages");
 			const response = await axios.get(path);
 			setMessages(response.data);
-			localStorage.setItem("messages", JSON.stringify(response.data));
+			localStorage.setItem("messages", JSON.stringify(response.data)); 
 		} else {
 			console.log("using local storage");
 			setMessages(JSON.parse(tmpMsg));
 		}
 		//Add message into local storage
 	};
-	//Check login status, if not login, redirect back to login page
 
 	//Function to send message
 	const handleSendMessage = async (e) => {
@@ -51,7 +56,52 @@ const chats = () => {
 
 	useEffect(() => {
 		fetchMessages();
+		document.addEventListener("scroll", trackScrolling);
+		return () => {
+			document.removeEventListener("scroll", trackScrolling);
+		};
 	}, []);
+
+	//Handle infinite scroll
+
+	const isBottom = ([e]) => {
+        if(e === undefined) {
+            // console.log("e is undefined");
+        }
+		return e.getBoundingClientRect().bottom <= window.innerHeight;
+	};
+
+    const isTop = ([e]) => {
+        if(e === undefined) {
+            // console.log("e is undefined");
+        }
+        // console.log(e.getBoundingClientRect().top);
+		return e.getBoundingClientRect().top === 0;
+	};
+
+    const refAssignCallback = ([e]) => {
+        if (e) {
+          //the containerRef is currently null, ref available = mounted.
+          var element = e;
+          var scrollWidth = element.scrollWidth;
+          var clientWidth = element.getBoundingClientRect().width;
+
+        } 
+    }
+
+	const trackScrolling = () => {
+		const wrappedElement = window.document.getElementsByClassName("chat-container");
+        // console.log(wrappedElement);
+		if (isBottom(wrappedElement)) {
+			console.log("header bottom reached");
+			// document.removeEventListener("scroll", trackScrolling);
+		} 
+
+        if (isTop(wrappedElement)) {
+            console.log("header top reached");
+            // document.removeEventListener("scroll", trackScrolling);
+        }
+	};
 
 	return (
 		<div>
@@ -63,7 +113,7 @@ const chats = () => {
 					})}
 				</div>
 				{/* Refractor this into component */}
-				<div className='chat-input'>
+				<div className='chat-input' ref = {divRef}>
 					<ChatInputs
 						handleSendMessage={handleSendMessage}
 						sendMessage={sendMessage}
